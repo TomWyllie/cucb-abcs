9c9644a2938cb646e3fa936c81867eefa3f5392d
Tom Wyllie <wyllietom@gmail.com>
2020-11-30 16:35:06 +0000

    Added updates-only generating script for treble and alto

---
+++ make_update.sh
@@ -0,0 +1,3 @@
+pdftk MERGED.pdf cat 2 4 7 9 22 25 28 29 41 42 45 46 61 77 80 81 89 10 11 26 27 56 57 79 output UPDATES_TREBLE.pdf
+pdftk rendered-alto/cucb_all_sets_alto.pdf cat 2 4 7 9 22 25 28 29 41 42 45 46 61 77 80 81 89 10 11 26 27 56 57 79 output UPDATES_ALTO.pdf
+
